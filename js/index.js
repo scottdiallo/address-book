d27ba3f518b0735d924ae977a42b62b238e4ff28
Scott Diallo <diallot@me.com>
2016-05-30 11:12:54 -0700

    address book layout done and all dependencies have been downloaded

---
+++ js/index.js
@@ -0,0 +1,15 @@
+'use strict'
+// Global variables declartions
+
+$(document).ready(function () {
+    console.log('hello world');
+    //hide contact form on page load
+    $('.contactDetails').hide();
+
+    $('#addSign').on('click', function () {
+        $('#addSign').hide();
+        $('.contactDetails').show();
+    })
+
+
+});
