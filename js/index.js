bd1649c4ad41adc2bbcfdb5e21ade04e646f4fce
Scott Diallo <diallot@me.com>
2016-05-31 00:13:24 -0700

    focus event working for entire form

--- js/index.js
+++ js/index.js
@@ -13,13 +13,51 @@ $(document).ready(function () {
 
     //function show field name when on focus andhide when blur
     $(".firstNameE").hide();
-
     $(".firstName").focus(function () {
-            $(".firstNameE").show();
-        })
-        //    console.log('hi');
+        $(".firstNameE").show();
+    })
     $(".firstName").blur(function () {
         $(".firstNameE").hide();
+    });
+    // event for last name
+    $(".lastNameE").hide();
+    $(".lastName").focus(function () {
+        $(".lastNameE").show();
+    })
+    $(".lastName").blur(function () {
+        $(".lastNameE").hide();
+    });
+    // event for phone number
+    $(".phoneNumberE").hide();
+    $(".phoneNbr").focus(function () {
+        $(".phoneNumberE").show();
+    })
+    $(".phoneNbr").blur(function () {
+        $(".phoneNumberE").hide();
+    });
+    // event for email
+    $(".emailE").hide();
+    $(".email").focus(function () {
+        $(".emailE").show();
+    })
+    $(".email").blur(function () {
+        $(".emailE").hide();
+    });
+    // event for gitHub
+    $(".gitHubE").hide();
+    $(".gitHub").focus(function () {
+        $(".gitHubE").show();
+    })
+    $(".gitHub").blur(function () {
+        $(".gitHubE").hide();
+    });
+    // event for location
+    $(".locationE").hide();
+    $(".location").focus(function () {
+        $(".locationE").show();
+    })
+    $(".location").blur(function () {
+        $(".locationE").hide();
     });
 
 });
