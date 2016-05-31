d088f2d1cf1224eba5c8c4941836a59a19687158
Scott Diallo <diallot@me.com>
2016-05-30 23:32:08 -0700

    blur and onfocus event added to first-name input field

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 // Global variables declartions
 
 $(document).ready(function () {
+    //    console.log('hi');
     console.log('hello world');
     //hide contact form on page load
     $('.contactDetails').hide();
@@ -11,5 +12,15 @@ $(document).ready(function () {
         $('.contactDetails').show();
     })
 
+    //function show field name when on focus andhide when blur
+    $(".focusEvent").hide();
+
+    $("input").focus(function () {
+        $(".focusEvent").show();
+    })
+
+    $("input").blur(function () {
+        $(".focusEvent").hide();
+    });
 
 });
